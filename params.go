0338e881b20db1d0227fd6b71fb6426132a8691b
Henry Skelsey <nskelsey@gmail.com>
2015-02-01 11:57:59 -0500

    Updated btcnet params. 22228 is the new network port. Still need to change blocks

--- params.go
+++ params.go
@@ -98,7 +98,7 @@ type Params struct {
 var MainNetParams = Params{
 	Name:        "mainnet",
 	Net:         btcwire.MainNet,
-	DefaultPort: "8333",
+	DefaultPort: "22228",
 
 	// Chain parameters
 	GenesisBlock:           &genesisBlock,
@@ -109,23 +109,7 @@ var MainNetParams = Params{
 	ResetMinDifficulty:     false,
 
 	// Checkpoints ordered from oldest to newest.
-	Checkpoints: []Checkpoint{
-		{11111, newShaHashFromStr("0000000069e244f73d78e8fd29ba2fd2ed618bd6fa2ee92559f542fdb26e7c1d")},
-		{33333, newShaHashFromStr("000000002dd5588a74784eaa7ab0507a18ad16a236e7b1ce69f00d7ddfb5d0a6")},
-		{74000, newShaHashFromStr("0000000000573993a3c9e41ce34471c079dcf5f52a0e824a81e7f953b8661a20")},
-		{105000, newShaHashFromStr("00000000000291ce28027faea320c8d2b054b2e0fe44a773f3eefb151d6bdc97")},
-		{134444, newShaHashFromStr("00000000000005b12ffd4cd315cd34ffd4a594f430ac814c91184a0d42d2b0fe")},
-		{168000, newShaHashFromStr("000000000000099e61ea72015e79632f216fe6cb33d7899acb35b75c8303b763")},
-		{193000, newShaHashFromStr("000000000000059f452a5f7340de6682a977387c17010ff6e6c3bd83ca8b1317")},
-		{210000, newShaHashFromStr("000000000000048b95347e83192f69cf0366076336c639f9b7228e9ba171342e")},
-		{216116, newShaHashFromStr("00000000000001b4f4b433e81ee46494af945cf96014816a4e2370f11b23df4e")},
-		{225430, newShaHashFromStr("00000000000001c108384350f74090433e7fcf79a606b8e797f065b130575932")},
-		{250000, newShaHashFromStr("000000000000003887df1f29024b06fc2200b55f8af8f35453d7be294df2d214")},
-		{267300, newShaHashFromStr("000000000000000a83fbd660e918f218bf37edd92b748ad940483c7c116179ac")},
-		{279000, newShaHashFromStr("0000000000000001ae8c72a0b0c301f67e3afca10e819efa9041e458e9bd7e40")},
-		{300255, newShaHashFromStr("0000000000000000162804527c6e9b9f0563a280525f9d08c12041def0a0f3b2")},
-		{319400, newShaHashFromStr("000000000000000021c6052e9becade189495d1c539aa37c58917305fd15f13b")},
-	},
+	Checkpoints: []Checkpoint{},
 
 	// Reject version 1 blocks once a majority of the network has upgraded.
 	// 95% (950 / 1000)
@@ -141,7 +125,7 @@ var MainNetParams = Params{
 	CoinbaseBlockHeightNumToCheck:  1000,
 
 	// Mempool parameters
-	RelayNonStdTxs: false,
+	RelayNonStdTxs: true,
 
 	// Address encoding magics
 	PubKeyHashAddrID: 0x00, // starts with 1
@@ -160,102 +144,12 @@ var MainNetParams = Params{
 // RegressionNetParams defines the network parameters for the regression test
 // Bitcoin network.  Not to be confused with the test Bitcoin network (version
 // 3), this network is sometimes simply called "testnet".
-var RegressionNetParams = Params{
-	Name:        "regtest",
-	Net:         btcwire.TestNet,
-	DefaultPort: "18444",
-
-	// Chain parameters
-	GenesisBlock:           &regTestGenesisBlock,
-	GenesisHash:            &regTestGenesisHash,
-	PowLimit:               regressionPowLimit,
-	PowLimitBits:           0x207fffff,
-	SubsidyHalvingInterval: 150,
-	ResetMinDifficulty:     true,
-
-	// Checkpoints ordered from oldest to newest.
-	Checkpoints: nil,
-
-	// Reject version 1 blocks once a majority of the network has upgraded.
-	// 75% (75 / 100)
-	// This is part of BIP0034.
-	BlockV1RejectNumRequired: 75,
-	BlockV1RejectNumToCheck:  100,
-
-	// Ensure coinbase starts with serialized block heights for version 2
-	// blocks or newer once a majority of the network has upgraded.
-	// 51% (51 / 100)
-	// This is part of BIP0034.
-	CoinbaseBlockHeightNumRequired: 51,
-	CoinbaseBlockHeightNumToCheck:  100,
-
-	// Mempool parameters
-	RelayNonStdTxs: true,
-
-	// Address encoding magics
-	PubKeyHashAddrID: 0x6f, // starts with m or n
-	ScriptHashAddrID: 0xc4, // starts with 2
-	PrivateKeyID:     0xef, // starts with 9 (uncompressed) or c (compressed)
-
-	// BIP32 hierarchical deterministic extended key magics
-	HDPrivateKeyID: [4]byte{0x04, 0x35, 0x83, 0x94}, // starts with tprv
-	HDPublicKeyID:  [4]byte{0x04, 0x35, 0x87, 0xcf}, // starts with tpub
-
-	// BIP44 coin type used in the hierarchical deterministic path for
-	// address generation.
-	HDCoinType: 1,
-}
+var RegressionNetParams = MainNetParams
 
 // TestNet3Params defines the network parameters for the test Bitcoin network
 // (version 3).  Not to be confused with the regression test network, this
 // network is sometimes simply called "testnet".
-var TestNet3Params = Params{
-	Name:        "testnet3",
-	Net:         btcwire.TestNet3,
-	DefaultPort: "18333",
-
-	// Chain parameters
-	GenesisBlock:           &testNet3GenesisBlock,
-	GenesisHash:            &testNet3GenesisHash,
-	PowLimit:               testNet3PowLimit,
-	PowLimitBits:           0x1d00ffff,
-	SubsidyHalvingInterval: 210000,
-	ResetMinDifficulty:     true,
-
-	// Checkpoints ordered from oldest to newest.
-	Checkpoints: []Checkpoint{
-		{546, newShaHashFromStr("000000002a936ca763904c3c35fce2f3556c559c0214345d31b1bcebf76acb70")},
-	},
-
-	// Reject version 1 blocks once a majority of the network has upgraded.
-	// 75% (75 / 100)
-	// This is part of BIP0034.
-	BlockV1RejectNumRequired: 75,
-	BlockV1RejectNumToCheck:  100,
-
-	// Ensure coinbase starts with serialized block heights for version 2
-	// blocks or newer once a majority of the network has upgraded.
-	// 51% (51 / 100)
-	// This is part of BIP0034.
-	CoinbaseBlockHeightNumRequired: 51,
-	CoinbaseBlockHeightNumToCheck:  100,
-
-	// Mempool parameters
-	RelayNonStdTxs: true,
-
-	// Address encoding magics
-	PubKeyHashAddrID: 0x6f, // starts with m or n
-	ScriptHashAddrID: 0xc4, // starts with 2
-	PrivateKeyID:     0xef, // starts with 9 (uncompressed) or c (compressed)
-
-	// BIP32 hierarchical deterministic extended key magics
-	HDPrivateKeyID: [4]byte{0x04, 0x35, 0x83, 0x94}, // starts with tprv
-	HDPublicKeyID:  [4]byte{0x04, 0x35, 0x87, 0xcf}, // starts with tpub
-
-	// BIP44 coin type used in the hierarchical deterministic path for
-	// address generation.
-	HDCoinType: 1,
-}
+var TestNet3Params = MainNetParams
 
 // SimNetParams defines the network parameters for the simulation test Bitcoin
 // network.  This network is similar to the normal test network except it is
@@ -264,49 +158,7 @@ var TestNet3Params = Params{
 // which are specifically specified are used to create the network rather than
 // following normal discovery rules.  This is important as otherwise it would
 // just turn into another public testnet.
-var SimNetParams = Params{
-	Name:        "simnet",
-	Net:         btcwire.SimNet,
-	DefaultPort: "18555",
-
-	// Chain parameters
-	GenesisBlock:           &simNetGenesisBlock,
-	GenesisHash:            &simNetGenesisHash,
-	PowLimit:               simNetPowLimit,
-	PowLimitBits:           0x207fffff,
-	SubsidyHalvingInterval: 210000,
-	ResetMinDifficulty:     true,
-
-	// Checkpoints ordered from oldest to newest.
-	Checkpoints: nil,
-
-	// Reject version 1 blocks once a majority of the network has upgraded.
-	// 75% (75 / 100)
-	BlockV1RejectNumRequired: 75,
-	BlockV1RejectNumToCheck:  100,
-
-	// Ensure coinbase starts with serialized block heights for version 2
-	// blocks or newer once a majority of the network has upgraded.
-	// 51% (51 / 100)
-	CoinbaseBlockHeightNumRequired: 51,
-	CoinbaseBlockHeightNumToCheck:  100,
-
-	// Mempool parameters
-	RelayNonStdTxs: true,
-
-	// Address encoding magics
-	PubKeyHashAddrID: 0x3f, // starts with S
-	ScriptHashAddrID: 0x7b, // starts with s
-	PrivateKeyID:     0x64, // starts with 4 (uncompressed) or F (compressed)
-
-	// BIP32 hierarchical deterministic extended key magics
-	HDPrivateKeyID: [4]byte{0x04, 0x20, 0xb9, 0x00}, // starts with sprv
-	HDPublicKeyID:  [4]byte{0x04, 0x20, 0xbd, 0x3a}, // starts with spub
-
-	// BIP44 coin type used in the hierarchical deterministic path for
-	// address generation.
-	HDCoinType: 115, // ASCII for s
-}
+var SimNetParams = MainNetParams
 
 var (
 	// ErrDuplicateNet describes an error where the parameters for a Bitcoin
